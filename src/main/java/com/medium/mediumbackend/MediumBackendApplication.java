1e3e373509934d479711c348803d06c3337b7d0d
rohith <rohith.chilupuri@zemosolabs.com>
2020-12-02 19:20:29 +0530

    Project structure

---
+++ src/main/java/com/medium/mediumbackend/MediumBackendApplication.java
@@ -0,0 +1,13 @@
+package com.medium.mediumbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediumBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediumBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medium/mediumbackend/MediumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medium.mediumbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediumBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
